981cca22c5aec5b97efbbd6159d21b96a557f49a
dSupertramp <salvatoredanilopalumbo@gmail.com>
2024-05-27 11:55:26 +0200

    astro-config

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,7 @@ import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import sitemap from "@astrojs/sitemap";
 export default defineConfig({
-  site: 'https://dsupertramp.github.io',
+  site: 'https://dSupertramp.github.io',
   output: "static",
   integrations: [tailwind(), sitemap(), ]
 });
